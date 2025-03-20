676b7602402d2288acb70b46cd7519988ececc54
tezzjob <tezzjob.services@gmail.com>
2025-03-21 01:25:36 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  base: "/", // Ensures correct path resolution
+  build: {
+    outDir: "dist", // Ensure Vite outputs to "dist"
+  },
 });
